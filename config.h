36bcdd5aced288c847bcd977d331b1b295356b92
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-10-20 18:22:45 +0100

    add comments

--- config.h
+++ config.h
@@ -1,9 +1,17 @@
 #define TRUE 1
 #define FALSE 0
 #define STDIN 0
-#define ASPORT "58011"
-#define PDPORT "5000"
-#define FSPORT "1"
+
+//IP address of the machine where the AS runs
 #define ASIP "localhost"
+// well-known port where the AS accepts requests
+// 58000 + GN --> 58022
+#define ASPORT "58022"
+
 #define PDIP "localhost"
+// well-known port where the PD runs an UDP server to accept future AS messages with verification codes
+// 57000 + GN --> 57022
+#define PDPORT "57022"
+
 #define FSIP "localhost"
+#define FSPORT "1"
