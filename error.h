579285c8cc4ffa25a5289b1aa5b86a557f0e0b4b
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-11-05 11:23:57 +0000

    fix missing format change

--- error.h
+++ error.h
@@ -1,8 +1,8 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-void errorExit(char* errorMessage);
-void errorDIYexit(char* errorMessage);
-void printError(char* errorMessage);
+void errorExit(char *errorMessage);
+void errorDIYexit(char *errorMessage);
+void printError(char *errorMessage);
 
 #endif
