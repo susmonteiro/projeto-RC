2501ce68546f0a9841763998c8c156c731b16caf
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-11-05 14:41:04 +0000

    fix missing imports

--- connection.h
+++ connection.h
@@ -2,6 +2,8 @@
 #define CONNECTION_H
 
 #include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 void udpOpenConnection(char *port, int *fd, struct addrinfo **res);
 void udpConnect(char *ip, char *port, int *fd, struct addrinfo **res);
