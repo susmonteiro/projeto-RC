4220e9345e1b81699596f7d53b7987b5fa0c4a4e
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-11-08 23:29:21 +0000

    fix error message printing when errno is 0

--- error.c
+++ error.c
@@ -5,7 +5,10 @@
 #include <string.h>
 
 void errorExit(char *errorMessage) {
-    printf("ERR: %s: %s\nExiting...\n", errorMessage, strerror(errno));
+    if (errno != 0)
+        printf("ERR: %s: %s\nExiting...\n", errorMessage, strerror(errno));
+    else
+        printf("ERR: %s\nExiting...\n", errorMessage);
     exit(1);
 }
 
@@ -15,5 +18,8 @@ void errorDIYexit(char *errorMessage) {
 }
 
 void printError(char *errorMessage) {
-    printf("ERR: %s: %s\nNot exiting...\n", errorMessage, strerror(errno));
+    if (errno != 0)
+        printf("ERR: %s: %s\nNot exiting...\n", errorMessage, strerror(errno));
+    else
+        printf("ERR: %s\nNot exiting...\n", errorMessage);
 }
