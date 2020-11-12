2bc8786c96d412c147338b92d297aeacced21f46
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2020-11-12 22:37:15 +0000

    debug

--- AS.c
+++ AS.c
@@ -305,7 +305,7 @@ char *validateOperation(char *uid, char *tid) {
             }
         }
     }
-    printf("TID: %d\t FS_TID: %d\t\n", tid, requests[i]->tid);
+    printf("TID: %d\t FS_TID: %d\t\n", tid, requests[i]->tid); //DEBUG
 
     if (fop != 'E') fop = requests[i]->fop[0];
 
