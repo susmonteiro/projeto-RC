486f9e0a8f677178683268e9b18b6046dd2bb1fc
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-11-14 20:49:24 +0000

    move error functions to each program

--- FS.c
+++ FS.c
@@ -1,6 +1,5 @@
 #include "config.h"
 #include "connection.h"
-#include "error.h"
 #include <arpa/inet.h>
 #include <dirent.h>
 #include <errno.h>
@@ -20,6 +19,8 @@
 #define MINARGS 1
 
 #define MAX(a, b) a *(a > b) + b *(b >= a)
+
+void exitFS();
 
 typedef struct user {
     int fd;
@@ -59,6 +60,23 @@ char fsport[8], asport[8], asip[32];
 // print if verbose mode
 void printv(char *message) { // TODO move to common file
     if (verbose == TRUE) printf("%s\n", message);
+}
+
+/*      === error functions ===       */
+
+void errorExit(char *errorMessage) {
+    if (errno != 0)
+        printf("ERR: %s: %s\n", errorMessage, strerror(errno));
+    else
+        printf("ERR: %s\n", errorMessage);
+    exitFS();
+}
+
+void printError(char *errorMessage) {
+    if (errno != 0)
+        printf("ERR: %s: %s\nNot exiting...\n", errorMessage, strerror(errno));
+    else
+        printf("ERR: %s\nNot exiting...\n", errorMessage);
 }
 
 void readGarbage(int fd, int size) {
@@ -247,7 +265,7 @@ void retrieveFile(int fd, Transaction transaction) {
     sprintf(message, "file %s retrieved for user %s", transaction->fname, transaction->uid);
     printv(message);
 
-    sprintf(message, "RRT OK %d ", fsize+1);
+    sprintf(message, "RRT OK %d ", fsize + 1);
     n = write(fd, message, strlen(message));
     if (n == -1) errorExit("write()");
 
